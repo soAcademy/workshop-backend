28df067419ba3f65841756c4d38580001c0329bf
Bond Rungrot <st122150@ait.asia>
2023-02-21 14:20:34 +0700

    activity 3

--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,21 @@ app.post("/post-query", (req: Request, res: Response) => {
   res.send(body);
 });
 
-// app.get("/get-sum", (req: Request, res: Response) => {
-//   const query = req.query;
-//   const number1= Number(query?.number1);
-//   const number2= Number(query?.number2);
-//   const sum = number1 + number2;
-//   res.send(`&{sum}`)
-// });
+app.get("/get-sum", (req: Request, res: Response) => {
+  const query = req.query;
+  const number1= Number(query?.number1);
+  const number2= Number(query?.number2);
+  const sum = number1 + number2;
+  res.send(`${sum}`)
+});
 
-// app.post("/post-sum", (req: Request, res: Response) => {
-//   const body = req?.body;
-//   const number1= Number(body?.number1);
-//   const number2= Number(body?.number2);
-//   const sum = number1 + number2;
-//   res.send(`&{sum}`)
-// });
+app.post("/post-sum", (req: Request, res: Response) => {
+  const body = req?.body;
+  const number1= Number(body?.number1);
+  const number2= Number(body?.number2);
+  const sum = number1 + number2;
+  res.send(`${sum}`)
+});
 
 app.listen(3200, () => {
   console.log("Server start on port 3200!");
