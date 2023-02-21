a995850b895d6bc14f97a340ea9aa1e5800a6740
jaiieth <midzty.rb128@gmail.com>
2023-02-21 10:55:21 +0700

    feat:add paths

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,25 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-app.get("/user/register", (req: Request, res: Response) => {
-  // res.send("Hello World 5555");
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello World 5555");
+});
+app.get("/user", (req: Request, res: Response) => {
+  res.json([
+    {name: "Earth",
+    age: 27
+  },{
+    name: "Pete",
+    age: 105
+  }])
+});
+app.get("/user/earth", (req: Request, res: Response) => {
   res.json({
     name: "Earth",
     age: 27,
   });
-  // res.status(200).send('Good')
-  // res.status(500).send('Error')
 });
+
 
 app.listen(5555, () => {
   console.log("Server start on port 5555!");
