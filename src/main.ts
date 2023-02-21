1d76b05f1f9cd38e94a73f256bae63324f4e0e37
Phisit Siprasatthong <phisite@gmail.com>
2023-02-21 14:00:35 +0700

    feat: add remaining solution for simpleApi #3 (GET/POST queries with numeral responses)

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,28 @@ app.post("/post-query", (req: Request, res: Response) => {
   res.send(body);
 });
 
+app.get("/get-sum", (req: Request, res: Response) => {
+  const query = req?.query;
+  const number1 = Number(query?.number1);
+  const number2 = Number(query?.number2);
+  const sum = number1 + number2;
+
+  // res.send(sum);
+  // res.send(String(sum));
+  res.send(`${sum}`);
+});
+
+app.post("/post-sum", (req: Request, res: Response) => {
+  const body = req?.body;
+  const number1 = Number(body?.number1);
+  const number2 = Number(body?.number2);
+  const sum = number1 + number2;
+
+  // res.send(sum);
+  // res.send(String(sum));
+  res.send(`${sum}`);
+});
+
 app.listen(8000, () => {
   console.log("Server start on port 8000!");
 });
