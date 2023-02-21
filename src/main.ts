5af9a5c7799c24b46c1fa6f7181ad54a091de838
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-21 13:48:06 +0700

    feat: simple API get,post send object and var

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,26 @@
 import express,{Application,Request,Response} from "express";
 
 const app: Application = express();
-app.post("/hello",(req: Request, res: Response)=>{
-  res.send("Hi ,Boeing747");
-});
-app.post("/hello2",(req: Request, res: Response)=>{
-  res.send("Hi ,Boeing777");
-});
 
-app.post("/post1",(req: Request, res: Response)=>{
-  const number1 =15;
-  res.send(`API1: ${number1}`)
-});
+app.use(express.json());
 
-app.post("/post2",(req:Request, res:Response)=>{
-  const numbers=[1,2,3,4,5,6,7,8,9,10];
-  const sum = numbers.reduce((acc,r)=> acc + r,0);
-  res.send(`API2: ${sum}`)  //เหมาะกับการส่งเป็น string
+app.get("/get-query",(req:Request, res:Response) => {
+  const query = req?.query;
+  console.log(query);
+  res.send(query);
+  //เทสใน path บน browser ได้ & post man
+  // http://localhost:4000/get-query?var1=123&var2=boeing&var3=mike
 })
 
-app.post("/post3",(req:Request, res:Response)=>{
-  const profile = {
-    name: 'Boeing',
-    location: 'Bangkok',
-    age: 7
-  };
-  res.json(profile) // ถ้าก้อนที่เราจะส่งไปเป็น {} object เราจะต้อง send เป็น .json 
-})
+app.post("/post-query", (req:Request, res:Response) => {
+  const body = req.body
+  console.log(body);
+  res.send(body);
+  //post รับเป็น body
+  //เทสใน post man เท่านั้น
+  // body > raw > json
+}) 
 
-app.post("/post4",(req:Request, res:Response)=>{
-  const products = [
-    {
-      name: "Collagen",
-      price: 390
-    },
-    {
-      name: "Vit C",
-      price: 290
-    }
-  ]
-  res.json(products)
-})
-
-app.post("/post5",(req:Request, res:Response)=>{
-  const products = [
-    {
-      name: "Collagen",
-      price: 390
-    },
-    {
-      name: "Vit C",
-      price: 290
-    }
-  ]
-  res.send(products)
-})
 
 app.listen(4000, ()=>{
   console.log("Sever start on port 4000");
