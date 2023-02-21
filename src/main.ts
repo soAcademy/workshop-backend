e7b7746f33222b51838a08cb4e9eaf1e4b45cb88
korana <korayut@gmail.com>
2023-02-21 10:42:24 +0700

    feat: add script in package.json

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from "express()"
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Kor");
+});
+
+app.listen(3100, () => {
+  console.log("Server start on port 3100");
+})
