7bbc80110e08d608b7e5d2d333bc9f8d2c412234
Firstweera <firstweera357@gmail.com>
2023-02-21 10:53:10 +0700

    feat:new path

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello!");
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello First!");
+});
+
+app.get("/hello2", (req: Request, res: Response) => {
+  res.send("Hello P'Bin!");
 });
 
 app.listen(3000, () => {
