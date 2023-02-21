c69f23cd533c0c5cc26fdbe9c61ea19bf93f23e6
BornDhamachart <dhamachart.c@gmail.com>
2023-02-21 13:09:59 +0700

    feat:Submit workshop

--- src/simpleApi/main1.ts
+++ src/simpleApi/main1.ts
@@ -2,27 +2,27 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-app.post("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello Born!");
 });
 
-app.post("/post1", (req: Request, res: Response) => {
+app.get("/get1", (req: Request, res: Response) => {
   const number1 = 15;
   res.send(`API1: ${number1}`);
 });
 
-app.post("/post2", (req: Request, res: Response) => {
+app.get("/get2", (req: Request, res: Response) => {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const sum = numbers.reduce((acc, r) => acc + r, 0);
   res.send(`API2: ${sum}`);
 });
 
-app.post("/post3", (req: Request, res: Response) => {
+app.get("/get3", (req: Request, res: Response) => {
   const profile = { name: "Bin", location: "Bangkok", age: 30 };
   res.send(profile);
 });
 
-app.post("/post4", (req: Request, res: Response) => {
+app.get("/get4", (req: Request, res: Response) => {
   const products = [
     { name: "Collagen", price: 390 },
     { name: "Vit C", price: 290 },
@@ -30,7 +30,7 @@ app.post("/post4", (req: Request, res: Response) => {
   res.send(products);
 });
 
-app.post("/post5", (req: Request, res: Response) => {
+app.get("/get5", (req: Request, res: Response) => {
   const products = [
     { name: "Collagen", price: 390 },
     { name: "Vit C", price: 290 },
