94e71ee0f4577c18089e1f0d8ba767f7053f0f58
BornDhamachart <dhamachart.c@gmail.com>
2023-02-21 13:34:17 +0700

    feat:Submit workshop

--- src/main.ts
+++ src/main.ts
@@ -2,40 +2,18 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-app.post("/", (req: Request, res: Response) => {
-  res.send("Hello Born!");
+app.use(express.json());
+
+app.get("/get-query", (req: Request, res: Response) => {
+  const query = req?.query;
+  console.log(query);
+  res.send(query);
 });
 
-app.post("/post1", (req: Request, res: Response) => {
-  const number1 = 15;
-  res.send(`API1: ${number1}`);
-});
-
-app.post("/post2", (req: Request, res: Response) => {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const sum = numbers.reduce((acc, r) => acc + r, 0);
-  res.send(`API2: ${sum}`);
-});
-
-app.post("/post3", (req: Request, res: Response) => {
-  const profile = { name: "Bin", location: "Bangkok", age: 30 };
-  res.send(profile);
-});
-
-app.post("/post4", (req: Request, res: Response) => {
-  const products = [
-    { name: "Collagen", price: 390 },
-    { name: "Vit C", price: 290 },
-  ];
-  res.send(products);
-});
-
-app.post("/post5", (req: Request, res: Response) => {
-  const products = [
-    { name: "Collagen", price: 390 },
-    { name: "Vit C", price: 290 },
-  ];
-  res.json(products);
+app.post("/post-query", (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body);
+  res.send(body);
 });
 
 app.listen(3100, () => {
