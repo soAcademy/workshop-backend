2e1de34c626aa935faae4e7f6d90f275a6557700
Firstweera <firstweera357@gmail.com>
2023-02-21 14:59:16 +0700

    feat: get status code2-4

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import axios from "axios";
 
 const app: Application = express();
 
@@ -18,6 +19,51 @@ app.post("/post-status-code", (req: Request, res: Response) => {
   body.name === "First"
     ? res.status(200).send(body)
     : res.status(500).send("Error: Name not found");
+});
+
+app.get("/get-status-code-2", (req: Request, res: Response) => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // const numbers = "[1, 2, 3, 4, 5]"; // อันนี้ Error typescript ไม่ให้ผ่าน
+  // const numbers = null; // อันนี้ Error typescript ไม่ให้ผ่าน
+  let sum;
+  try {
+    sum = numbers.reduce((acc, r) => acc + r, 0);
+    res.status(200).send(`${sum}`);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("ERROR");
+  }
+});
+
+
+// .then
+app.get("/get-status-code-3", (req: Request, res: Response) => {
+  axios({
+    url: "https://www.arifootballstore.com/", // status 200 ok
+    // url: "localhost:9999", // status 500 Internal Server Error
+  })
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+// async await
+app.get("/get-status-code-4", async (req: Request, res: Response) => {
+  try {
+    const response = await axios({
+      url: "https://www.iphonemod.net/", // status 200
+    });
+    console.log(response.data);
+    res.status(200).send(response.data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
 app.listen(3000, () => {
