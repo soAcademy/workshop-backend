a1e5a1ca96151e582f1bce4edf283333ccfe5c08
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-21 14:45:57 +0700

    feat: create API get-status-code2

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import express,{Application,Request,Response} from "express";
+import axios from "axios";
+
 const app: Application = express();
 app.use(express.json());
 
@@ -13,6 +15,21 @@ app.post("/post-status-code", (req:Request, res:Response) => {
   query.name === "Boeing"
   ? res.status(200).send(query) : res.status(500).send("server error");
 })
+
+app.get("/get-status-code2", (req:Request, res:Response) => {
+  const number = [1,2,3,4,5]; 
+   // const numbers = "[1, 2, 3, 4, 5]"; // อันนี้ Error typescript ไม่ให้ผ่าน
+  // const numbers = null; // อันนี้ Error typescript ไม่ให้ผ่าน
+ let sum;
+ try {
+  sum = number.reduce((acc,r)=> acc + r ,0);
+  res.status(200).send(`${sum}`)
+ } catch (e) {
+console.log(e);
+res.status(500).send("ERROR");
+ }
+});
+
 
 
 app.listen(4000, ()=>{
