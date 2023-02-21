d6538cb817de345e624a32e8e74ae605b9d27f77
jaiieth <midzty.rb128@gmail.com>
2023-02-21 10:46:49 +0700

    feat: '/user/register' respond json

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/user/register", (req: Request, res: Response) => {
+  // res.send("Hello World 5555");
+  res.json({
+    name: "Earth",
+    age: 27,
+  });
+  // res.status(200).send('Good')
+  // res.status(500).send('Error')
+});
+
+app.listen(5555, () => {
+  console.log("Server start on port 5555!");
+});
