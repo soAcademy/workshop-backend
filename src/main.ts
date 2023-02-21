edf60e82b277588489e0d5dca56a73e5d1770341
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-21 14:07:44 +0700

    feat:create simple API get-sum, post-sum

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,32 @@ app.post("/post-query", (req:Request, res:Response) => {
   // body > raw > json
 }) 
 
+app.get("/get-sum",(req:Request, res:Response)=>{
+  const query = req?.query;
+  console.log(query);
+  const number1 = Number(query?.number1);
+  const number2 = Number(query?.number2);
+  const sum = number1 + number2
+
+  res.send(`${sum}`) // รันได้ เพราะทำให้เป็น string
+   //http://localhost:4000/get-sum?number1=7&number2=4
+  //output 11
+})
+
+app.post("/post-sum", (req: Request, res: Response) => {
+  const body = req?.body; //body*
+  console.log(body);
+  const number1 = Number(body?.number1);
+  const number2 = Number(body?.number2);
+  const sum = number1 + number2;
+
+  // res.send(sum); // รันไม่ได้ เพราะคิดว่าเป็น status code
+  res.send(`${sum}`) // รันได้ เพราะทำให้เป็น string
+
+ 
+});
+
+
 
 app.listen(4000, ()=>{
   console.log("Sever start on port 4000");
