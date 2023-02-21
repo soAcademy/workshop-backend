d803a122a90eb1415c89890650b565c5be83de24
Bond Rungrot <st122150@ait.asia>
2023-02-21 15:18:30 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,67 @@
+// อันนี้เรียนเขียนการกำหนด Status code ให้เรารู้ว่ามันส่งค่าอะไรสำเร็จหรือไม่สำเร็จ
+// ลบพวกอันเก่าทิ้งไปได้เลย
 import express, { Application, Request, Response } from "express";
+import axios from "axios";
 const app: Application = express();
 
 app.use(express.json());
 
-app.get("/get-query", (req: Request, res: Response) => {
-  const query = req?.query;
-  res.send(query);
+// อันนี้ลืม yarn add axios
+// app.get("/get-status-code", (req: Request, res: Response) => {
+//   const query = req?.query;
+//   query.name === "Bond"
+//     ? res.status(200).send(query)
+//     : res.status(500).send("ERROR: NAME NOT FOUND");
+// });
+
+// อันนี้เป็นตัว Post ระวังดี ๆ เย็น ๆ
+// app.post("/post-status-code", (req: Request, res: Response) => {
+//   const body = req?.body;
+//   body.name === "Bond"
+//     ? res.status(200).send(body)
+//     : res.status(500).send("ERROR: NAME NOT FOUND");
+// });
+
+// ตัวนี้คือเรากำหนดตัวแปรมาเรียบร้ยอแล้ว ไม่ต้องไปใส่ค่าใน Postman
+// app.get("/get-status-code2", (req: Request, res: Response) => {
+//   const numbers = [1, 2, 3, 4, 5];
+//   let sum;
+//   try {
+//     sum = numbers.reduce((acc, r) => acc + r, 0);
+//     res.status(200).send(`${sum}`);
+//   } catch (e) {
+//     res.status(500).send("ERROR");
+//   }
+// });
+// response ไว้วตอนที่เราส่ง output ออกไป request คือตัวที่เราไว้ใช้ตอนเราขอ API เข้ามา
+// อันนี้เป็ API ยิงเข้า web bloc อันหนึ่งของไทย ถ้าอยากเห็นแบบพร้อมสีก็ให้ไปที่ preview แทนที่จะเป็น pretty
+// app.get("/get-status-code3", (req: Request, res: Response) => {
+//   axios({
+//     // url: "http://localhost:9999",
+//     url: "https://blognone.com",
+//   })
+//   .then((response)=>{
+//     res.status(200).send(response.data);
+//   })
+//   .catch((e) => {
+//     res.status(500).send("ERROR");
+//   });
+// });
+
+// อันนี้ทำเพื่อให้ดูว่า เราใช้ Error handling ให้เรารู้ว่ามันมีข้อผิดพลาดที่ไหน
+// ข้อผิดพลาดมันมีทั้งตัว token/ URL web ที่เราดึงมาที่มันปิดไป/ 
+app.get("/get-status-code4", async (req: Request, res: Response) => {
+  try {
+    const response = await axios ({
+      // url: "http://localhost:9999",
+      url: "https://blognone.com",
+    });
+    res.status(200).send(response.data);
+  } catch (e) {
+    res.status(500).send("ERROR");
+  }
 });
 
-app.post("/post-query", (req: Request, res: Response) => {
-  const body = req.body;
-  res.send(body);
-});
-
-app.get("/get-sum", (req: Request, res: Response) => {
-  const query = req.query;
-  const number1= Number(query?.number1);
-  const number2= Number(query?.number2);
-  const sum = number1 + number2;
-  res.send(`${sum}`)
-});
-
-app.post("/post-sum", (req: Request, res: Response) => {
-  const body = req?.body;
-  const number1= Number(body?.number1);
-  const number2= Number(body?.number2);
-  const sum = number1 + number2;
-  res.send(`${sum}`)
-});
 
 app.listen(3200, () => {
   console.log("Server start on port 3200!");
