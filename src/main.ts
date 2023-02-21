d3688ac17573c92343980eb6b0dde01f3271be31
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-21 15:01:12 +0700

    feat: create API /get-status-code4

--- src/main.ts
+++ src/main.ts
@@ -24,26 +24,40 @@ app.post("/post-status-code", (req:Request, res:Response) => {
 //  try {
 //   sum = number.reduce((acc,r)=> acc + r ,0);
 //   res.status(200).send(`${sum}`)
-//  } catch (e) {
-// console.log(e);
+//  } catch (err) {
+// console.log(err);
 // res.status(500).send("ERROR");
 //  }
 // });
 
-app.get("/get-status-code3", (req:Request, res:Response) => {
-  axios({
-    url:
+// app.get("/get-status-code3", (req:Request, res:Response) => {
+//   axios({
+//     url:
+//     // url: "http://localhost:9999",
+//     "https://blognone.com"
+//   })
+//   .then((response)=>{
+//     console.log(response.data);
+//     res.status(200).send(response.data)
+//   })
+//   .catch((err)=>{
+//     console.log(err);
+//     res.status(500).send(err);
+//   })
+// })
+
+app.get("/get-status-code4",async(req:Request, res:Response) => {
+ try {
+  const response = await axios({
     // url: "http://localhost:9999",
-    "https://blognone.com"
-  })
-  .then((response)=>{
-    console.log(response.data);
-    res.status(200).send(response.data)
-  })
-  .catch((err)=>{
-    console.log(err);
-    res.status(500).send(err);
-  })
+    url: "https://blognone.com",
+  });
+  console.log(response.data);
+  res.status(200).send(response.data);
+ } catch (err) {
+  console.log(err);
+  res.status(500).send(err);
+ }
 })
 
 
