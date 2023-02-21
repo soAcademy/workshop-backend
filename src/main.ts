8ed18d2d8131b26ef8016951631487aa1d1502f9
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-22 03:39:57 +0700

    feat: create the Type and use it for express server

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,130 @@
 import express,{Application,Request,Response} from "express";
-import axios from "axios";
+
+
+const temp : number = 3
+const tempObj:{name:string,age:number,option?:string} = {name:'abc',age:123,option:'adf'}
+//สร้าง function
+
+const add = (x:number,y:number)=> x+y;
+
+// Hello At
+const helloAt = (args: {name: string; location:string}) => ({
+  text: `Hello ${args.name} at ${args.location}`,
+  createdAt: new Date()
+})
+
+// Hello Sum
+const helloSum = (args: {
+ name: string;
+ number: {x: number; y:number; z:number};
+}) => ({
+  text: `Hello ${args.name} at ${args.number.x + args.number.y + args.number.z}`,
+  createAt: new Date()
+})
+
+interface IHelloMultiply {
+  name: string;
+  number: {
+    x: number; y:number; z: number
+  }
+}
+// Hello Multiply
+const helloMultiply = (args: IHelloMultiply) => ({
+  text: `Hello ${args.name} multiply ${
+    args.number.x * args.number.y * args.number.z
+  }`,
+  createAt: new Date()
+})
+
+//hello Reduce
+interface IHelloReduce {
+  name: string;
+  numbers: number[] | undefined;
+}
+
+const helloReduce = (args: IHelloReduce) => {
+  //typeof = การบอกชนิดของสิ่งที่เราวางไว้ต่อจากมันว่าสิ่งนั้นเป็น type อะไร ซึ่งมันจะบอก type ออกมาเป็น string
+  if (typeof args.numbers === 'object'){
+    const result = args.numbers.reduce((acc,r)=> acc + r,0);
+    return {
+      text: `Hello ${args.name} reduce ${result}`,
+      createAt: new Date()
+    }
+  }
+}
+
+//hello Order
+interface IHelloOrder {
+  name: string;
+  orders: {
+    product: string,
+    price: number
+  }[];
+}
+
+const helloOrder = (args: IHelloOrder) =>{
+  const result = args.orders.reduce((acc,r)=> acc + r.price, 0);
+  return {
+    text: `Hello ${args.name} order ${result}`,
+    createAt: new Date(),
+  }
+}
+// #####################################################
+//                   Express Server
+// #####################################################
+
 
 const app: Application = express();
 app.use(express.json());
 
-// สร้าง Nested Path
-
-app.get("/get/nested1", (req:Request,res:Response) => {
-  res.status(200).send("Nested1")
-});
-
-app.get("/get/nested2/:id",(req:Request,res:Response) => {
-  const params = req?.params
-  console.log(params);
-  res.status(200).send(`id: ${params.id}`)
-});
-
-app.get("/get/nested3/:id/:title",(req:Request,res:Response) => {
-  const params = req?.params
-  console.log(params);
-  res.status(200).send(`id:${params.id}, title: ${params.title}`)
+app.post("/function/add", (req:Request,res:Response)=>{
+  const body = req?.body;
+  const result = add(body?.x,body?.y);
+  res.status(200).send(`Result: ${result}`);
+  //.send = เพราะเป็น string
 })
 
-app.get("/get/nested5", (req: Request, res: Response) => {
-  const obj1 = {
-    a1: 1,
-    a2: 2,
-  };
-  const obj2 = {
-    b1: 11,
-    b2: 22,
-  };
-  res.status(200).json([obj1, obj2]);
-});
+app.post("/function/helloAt", (req:Request,res:Response)=>{
+  const body = req?.body;
+  const result = helloAt({
+    name: body?.name,
+    location: body?.location
+  });
+  res.status(200).json(result);
+})
 
+app.post("/function/helloSum",(req:Request, res:Response)=>{
+  const body = req?.body;
+  const result = helloSum({
+    name: body?.name,
+    number: {x:body?.number.x, y:body?.number.y, z:body?.number.z}
+  });
+  res.status(200).json(result)
+})
+
+app.post("/function/helloMultiply", (req:Request,res:Response)=>{
+  const body = req?.body;
+  const result = helloMultiply({
+    name: body?.name,
+    number: {x:body?.number.x, y:body?.number.y, z:body?.number.z}
+  });
+res.status(200).json(result)
+})
+
+app.post("/function/helloReduce", (req:Request,res:Response)=>{
+  const body = req.body;
+  const result = helloReduce({
+    name: body?.name,
+    numbers: body?.number
+  })
+  res.status(200).json(result)
+})
+
+app.post("/function/helloOrder", (req:Request, res:Response)=>{
+  const body = req?.body;
+  const result = helloOrder(body);
+  res.status(200).json(result);
+})
 
 app.listen(4000, ()=>{
   console.log("Sever start on port 4000");
---
+++ src/simpleAPI/main5.ts
@@ -0,0 +1,40 @@
+import express,{Application,Request,Response} from "express";
+
+
+const app: Application = express();
+app.use(express.json());
+
+// สร้าง Nested Path
+
+app.get("/get/nested1", (req:Request,res:Response) => {
+  res.status(200).send("Nested1")
+});
+
+app.get("/get/nested2/:id",(req:Request,res:Response) => {
+  const params = req?.params
+  console.log(params);
+  res.status(200).send(`id: ${params.id}`)
+});
+
+app.get("/get/nested3/:id/:title",(req:Request,res:Response) => {
+  const params = req?.params
+  console.log(params);
+  res.status(200).send(`id:${params.id}, title: ${params.title}`)
+})
+
+app.get("/get/nested5", (req: Request, res: Response) => {
+  const obj1 = {
+    a1: 1,
+    a2: 2,
+  };
+  const obj2 = {
+    b1: 11,
+    b2: 22,
+  };
+  res.status(200).json([obj1, obj2]);
+});
+
+
+app.listen(4000, ()=>{
+  console.log("Sever start on port 4000");
+})
