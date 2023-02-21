f3d53333eb50afb093f6d48959b7a5c46b6b6483
Panasun Sunanta <panasunsunanta@gmail.com>
2023-02-21 15:45:08 +0700

    feat: combined 2 objects to 1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,42 @@
 import express, { Application, Request, Response } from "express";
+import axios from "axios";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Bin!");
+// สร้าง Nested Path
+
+// ต้องเพิ่มบรรทัดนี้เพื่อให้ POST method รับข้อมูลแบบ json ได้
+app.use(express.json());
+
+app.get("/get/nested1", (req: Request, res: Response) => {
+   res.status(200).send("Nested1")
 });
 
-app.listen(3100, () => {
-  console.log("Server start on port 3100!");
+
+app.get("/get/nested2/:id", (req: Request, res: Response) => {
+  const params = req?.params
+  console.log(params)
+  res.status(200).send(`id: ${params?.id}`)
+});
+
+app.get("/get/nested3/:id/:title", (req: Request, res: Response) => {
+  const params = req?.params
+  console.log(params)
+  // res.status(200).send(`id: ${params?.id}, title: ${params?.title}`)
+  const obj1 = {
+    a1: 1,
+    a2: 2,
+  }
+  const obj2 = {
+    b1: 11,
+    b2: 22,
+  }
+  res.status(200).json([
+    obj1,
+    obj2
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("Server start on port 3000!");
 });
