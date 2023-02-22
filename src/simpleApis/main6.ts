884c2230dd1626d9f9452064bba44500a5dccc24
mplug <oppoalien@gmail.com>
2023-02-22 15:13:19 +0700

    feat: do backend ws main6 runtime check type manual

--- src/simpleApis/main6.ts
+++ src/simpleApis/main6.ts
@@ -6,25 +6,30 @@ const app: Application = express();
 app.use(express.json());
 
 //ex.1
+const isNumber = (data: any) => typeof data === "number";
 const add = (x: number, y: number) => x + y;
 app.post("/function/add", (req: Request, res: Response) => {
   const body = req?.body;
-  const result = add(body?.x, body?.y);
-  res.status(200).send(`Result: ${result}`);
+  isNumber(body?.x) && isNumber(body?.y)
+    ? res.status(200).send(`Result: ${add(body?.x, body?.y)}`)
+    : res.status(500).send(`ERROR: data mismatch!`);
 });
 
 //ex.2
+const isString = (data: any) => typeof data === "string";
 const helloAt = (args: { name: string; location: string }) => ({
   text: `Hello ${args.name} at ${args.location}`,
   createAt: new Date(),
 });
 app.post("/function/helloAt", (req: Request, res: Response) => {
   const body = req?.body;
-  const result = helloAt({ name: body?.name, location: body?.location });
-  res.status(200).json(result);
+  isString(body?.name) && isString(body?.location)
+    ? res.status(200).json(helloAt(body))
+    : res.status(500).json({ message: `ERROR: data mismatch!` });
 });
 
 //ex.3
+const isObject = (data: any) => typeof data === "object";
 const helloSum = (args: {
   name: string;
   number: { x: number; y: number; z: number };
@@ -36,11 +41,11 @@ const helloSum = (args: {
 });
 app.post("/function/helloSum", (req: Request, res: Response) => {
   const body = req?.body;
-  const result = helloSum({
-    name: body?.name,
-    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
-  });
-  res.json(result);
+  isString(body?.name) &&
+  isObject(body?.number) &&
+  Object.values(body?.number).every(isNumber)
+    ? res.status(200).json(helloSum(body))
+    : res.status(500).json({ message: `ERROR: data mismatch!` });
 });
 
 //ex.4
@@ -56,14 +61,15 @@ const helloMultiply = (args: IHelloMultiply) => ({
 });
 app.post("/function/helloMultiply", (req: Request, res: Response) => {
   const body = req?.body;
-  const result = helloMultiply({
-    name: body?.name,
-    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
-  });
-  res.json(result);
+  isString(body?.name) &&
+  isObject(body?.number) &&
+  Object.values(body?.number).every(isNumber)
+    ? res.status(200).json(helloMultiply(body))
+    : res.status(500).json({ message: `ERROR: data mismatch!` });
 });
 
 //ex.5
+const isArray = (data: any) => Array.isArray(data);
 interface IHelloReduce {
   name: string;
   numbers: number[];
@@ -77,11 +83,11 @@ const helloReduce = (args: IHelloReduce) => {
 };
 app.post("/function/helloReduce", (req: Request, res: Response) => {
   const body = req?.body;
-  const result = helloReduce({
-    name: body?.name,
-    numbers: body?.numbers,
-  });
-  res.json(result);
+  isString(body?.name) &&
+  isArray(body?.numbers) &&
+  body?.numbers.every(isNumber)
+    ? res.status(200).json(helloReduce(body))
+    : res.status(500).json({ message: `ERROR: data mismatch!` });
 });
 
 //ex.6
@@ -101,9 +107,13 @@ const helloOrder = (args: IHelloOrder) => {
 };
 app.post("/function/helloOrder", (req: Request, res: Response) => {
   const body = req?.body;
-  // console.log(body);
-  const result = helloOrder(body);
-  res.json(result);
+  isString(body?.name) &&
+  isArray(body?.orders) &&
+  body?.orders.every(isObject) &&
+  body?.orders.map((r: { product: any }) => r.product).every(isString) &&
+  body?.orders.map((r: { price: any }) => r.price).every(isNumber)
+    ? res.status(200).json(helloOrder(body))
+    : res.status(500).json({ message: `ERROR: data mismatch!` });
 });
 
 app.listen(3000, () => {
