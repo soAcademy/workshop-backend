c7a5025b221c50d284c85a45efb0f94714373e45
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-02-22 11:59:46 +0700

    feat:helloSum main6

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,17 @@ app.use(express.json());
 
 const add = (x: number, y: number) => x + y;
 
-const helloAt = (args:{name:string,location:string})=>({
+const helloAt = (args: { name: string; location: string }) => ({
   text: `${args.name} at ${args.location}`,
   creatAt: new Date(),
+});
+
+const helloSum = (args: {
+  name: string;
+  number: { x: number; y: number; z: number };
+}) => ({
+  text: `Hello ${args.name} ! sum ${args.number.x + args.number.y + args.number.z}`,
+  createdAt : new Date(),
 });
 
 app.post("/post/add", (req: Request, res: Response) => {
@@ -20,18 +28,25 @@ app.post("/post/add", (req: Request, res: Response) => {
 interface IF1Args {
   x: number;
   y: number;
-} 
+}
 
-app.post("/post/helloAt",(req:Request, res:Response)=>{
+app.post("/post/helloAt", (req: Request, res: Response) => {
   const body = req?.body;
   const result = helloAt({
     name: body?.name,
-    location: body?.location
-  })
+    location: body?.location,
+  });
   res.status(200).json(result);
+});
 
-})
-
+app.post("/post/helloSum",(req:Request, res:Response)=>{
+  const body = req?.body;
+  const result = helloSum({
+    name: body?.name,
+    number: {x: body?.number.x, y: body?.number.y,z: body?.number.z}
+  });
+  res.status(200).json(result);
+});
 
 
 app.listen(5000, () => {
