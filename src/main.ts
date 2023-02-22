63600b42fa312393f9bcd75b21b60136f7c1b59f
Panasun Sunanta <panasunsunanta@gmail.com>
2023-02-22 10:40:15 +0700

    feat: interface

--- src/main.ts
+++ src/main.ts
@@ -80,3 +80,60 @@ console.log(
     { a1: "world", a2: 20 },
   ])
 );
+
+interface IF10Args {
+  a1: string;
+  a2: number;
+}
+
+const f10 = (x: IF10Args[]) => x[0].a2 + x[1].a2;
+
+interface IF11Args {
+  a1: number | string;
+  a2: number | string;
+}
+
+const f11 = (x: IF11Args) => `${x.a1} ${x.a2}`;
+
+interface IF12Args {
+  num1: number[];
+}
+
+const f12 = (args: IF12Args) => args.num1.reduce((acc, r) => acc + r, 0);
+
+interface IF13Args {
+  a1: number;
+  a2: string;
+  a3: IF12Args;
+}
+
+const f13 = (args: IF13Args) => args.a3.num1.reduce((acc, r) => acc + r, 0);
+console.log(
+  f13({
+    a1: 10,
+    a2: "Bin",
+    a3: {
+      num1: [1, 2, 3],
+    },
+  })
+);
+
+interface IF14Args extends IF13Args {
+  a4: boolean;
+}
+
+const f14 = (args: IF14Args) => args.a3.num1.reduce((acc, r) => acc + r, 0);
+console.log(
+  f14({
+    a1: 10,
+    a2: "Bin",
+    a3: {
+      num1: [1, 2, 3],
+    },
+    a4: true,
+  })
+);
+
+interface IF15Args {
+  data: IF14Args[];
+}
