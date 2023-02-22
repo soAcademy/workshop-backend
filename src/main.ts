7612633be7253d64b3fc2849a5d9530786e66c0e
BornDhamachart <dhamachart.c@gmail.com>
2023-02-22 13:46:13 +0700

    feat:Submit workshop

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,35 @@ const helloMultipy = (args: IHelloMultipy) => ({
   createAt: new Date(),
 });
 
+interface IHelloReduce {
+  name: string;
+  number: number[];
+}
+
+const helloReduce = (args: IHelloReduce) => {
+  const reduceResult = args.number.reduce((acc, r) => acc + r, 0);
+  return {
+    text: `Hello ${args.name} Reduce = ${reduceResult}`,
+    createAt: new Date(),
+  };
+};
+
+interface IHelloOrder {
+  name: string;
+  orders: {
+    product: string;
+    price: number;
+  }[];
+}
+
+const helloOrder = (args: IHelloOrder) => {
+  const result = args.orders.reduce((acc, r) => acc + r.price, 0);
+  return {
+    text: `Hello ${args.name} order = ${result}`,
+    createAt: new Date(),
+  };
+};
+
 const app: Application = express();
 app.use(express.json());
 
@@ -63,6 +92,24 @@ app.post("/function/helloMultipy", (req: Request, res: Response) => {
   const result = helloMultipy({
     name: body?.name,
     number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloReduce", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloReduce({
+    name: body?.name,
+    number: body?.number,
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloOrder", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloOrder({
+    name: body?.name,
+    orders: body?.orders,
   });
   res.status(200).json(result);
 });
