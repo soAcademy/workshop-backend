3560f2ab14376aede58d54f0571e2ecb3ff291a9
Phisit Siprasatthong <phisite@gmail.com>
2023-02-22 13:59:17 +0700

    fix: split main.ts into main6.ts and main6_interface.ts

---
+++ src/simpleApi/main6.ts
@@ -0,0 +1,110 @@
+// import axios from "axios";
+import express, { Application, Request, Response } from "express";
+
+const add = (x: number, y: number) => x + y;
+
+const helloAt = (args: { name: string; location: string }) => ({
+  text: `Hello ${args.name} at ${args.location}`,
+  createdAt: new Date(),
+});
+
+const helloSum = (args: {
+  name: string;
+  number: { x: number; y: number; z: number };
+}) => ({
+  text: `Hello ${args.name} sum ${
+    args.number.x + args.number.y + args.number.z
+  }`,
+  createdAt: new Date(),
+});
+
+interface IHelloMultiply {
+  name: string;
+  number: { x: number; y: number; z: number };
+}
+
+const helloMultiply = (args: IHelloMultiply) => ({
+  text: `Hello ${args.name} multiply ${
+    args.number.x * args.number.y * args.number.z
+  }`,
+  createdAt: new Date(),
+});
+
+interface IHelloReduce {
+  name: string;
+  numbers: number[];
+}
+
+const helloReduce = (args: IHelloReduce) => {
+  const result = args.numbers.reduce((acc, n) => acc + n, 0);
+  return { text: `Hello ${args.name} reduce ${result}`, createdAt: new Date() };
+};
+
+interface IHelloOrder {
+  name: string;
+  orders: {
+    product: string;
+    price: number;
+  }[];
+}
+
+const helloOrder = (args: IHelloOrder) => {
+  const result = args.orders.reduce(
+    (accPrice, order) => accPrice + order.price,
+    0
+  );
+  return { text: `Hello ${args.name} order ${result}`, createdAt: new Date() };
+};
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.post("/function/add", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = add(body?.x, body?.y);
+  res.status(200).send(`Result: ${result}`);
+});
+
+app.post("/function/helloAt", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloAt({ name: body?.name, location: body?.location });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloSum", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloSum({
+    name: body?.name,
+    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloMultiply", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloMultiply({
+    name: body?.name,
+    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloReduce", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloReduce({
+    name: body?.name,
+    numbers: body?.numbers,
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloOrder", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloOrder(body);
+  res.status(200).json(result);
+});
+
+app.listen(8000, () => {
+  console.log("Server start on port 8000!");
+});
