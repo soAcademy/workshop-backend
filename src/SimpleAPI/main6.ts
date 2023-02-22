ff8b6be871ddcea5ea3d8108f478a75ffd8a001b
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-02-22 11:24:18 +0700

    main6

--- src/SimpleAPI/main6.ts
+++ src/SimpleAPI/main6.ts
@@ -4,12 +4,12 @@ import axios from "axios";
 const app: Application = express();
 app.use(express.json());
 
-const add = (x: number,y:number) => (x+y);
+const add = (x: number, y: number) => x + y;
 
-app.post("/post/add",(req:Request, res:Response)=>{
- const body = req?.body;
- const result = add(body?.x, body?.y);
- res.status(200).send(`Result ${result}`);
+app.post("/post/add", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = add(body?.x, body?.y);
+  res.status(200).send(`Result ${result}`);
 });
 
 app.listen(5000, () => {
