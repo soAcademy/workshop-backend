377640db11bcb85f44ee75ed84d3565a8229ee51
BornDhamachart <dhamachart.c@gmail.com>
2023-02-22 11:41:15 +0700

    feat:Submit workshop

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import express, { Application, Request, Response } from "express";
 import axios from "axios";
 
 const add = (x: number, y: number) => x + y;
+const helloAt = (args: { name: string; location: string }) => ({
+  text: `Hello ${args.name} at ${args.location}`,
+  createAt: new Date(),
+});
 
 const app: Application = express();
 app.use(express.json());
@@ -10,6 +14,15 @@ app.post("/function/add", (req: Request, res: Response) => {
   const body = req?.body;
   const result = add(body?.x, body?.y);
   res.status(200).send(`Result: ${result}`);
+});
+
+app.post("/function/helloAt", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloAt({
+    name: body?.name,
+    location: body?.location,
+  });
+  res.status(200).json(result);
 });
 
 app.listen(3100, () => {
