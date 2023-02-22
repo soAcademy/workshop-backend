ab32331fb59143e33f6896ffeadbe609006661e2
Firstweera <firstweera357@gmail.com>
2023-02-22 11:14:50 +0700

    feat: interface and force type

--- src/main.ts
+++ src/main.ts
@@ -144,3 +144,103 @@ console.log(
     { a1: "First", a2: 9 },
   ])
 );
+
+//////// interface
+
+// f10
+interface IF10Args {
+  a1: string;
+  a2: number;
+}
+
+const f10 = (x: IF10Args[]) => x[0].a2 + x[1].a2;
+
+console.log(
+  f10([
+    { a1: "Meow!!", a2: 8 },
+    { a1: "First", a2: 9 },
+  ])
+);
+
+// f11
+interface IF11Args {
+  a1: number | string;
+  a2: number | string;
+}
+
+const f11 = (x: IF11Args) => `${x.a1}${x.a2}`;
+
+console.log(f11({ a1: "First ", a2: 1 }));
+
+// f12
+interface IF12Args {
+  num1: number[];
+}
+
+const f12 = (x: IF12Args) => x.num1.reduce((acc, r) => acc + r, 0);
+
+console.log(f12({ num1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }));
+
+// f13
+interface IF13Args {
+  a1: number;
+  a2: string;
+  a3: IF12Args;
+}
+
+const f13 = (x: IF13Args) => x.a3.num1.reduce((acc, r) => acc + r, 0);
+
+console.log(f13({ a1: 10, a2: "First!", a3: { num1: [5, 6, 8, 97, 1] } }));
+
+// f14
+interface IF14Args extends IF13Args {
+  a4: boolean;
+}
+
+const f14 = (x: IF14Args) =>
+  x.a4 ? x.a3.num1.reduce((acc, r) => acc + r, 0) : false;
+
+console.log(
+  f14({ a1: 10, a2: "First!", a3: { num1: [5, 6, 8, 97, 1] }, a4: false })
+);
+
+// f15 ????
+// interface IF15Args {
+//   data: IF14Args[];
+// }
+
+// const f15 = (x: IF15Args) => x.data;
+
+// console.log(
+//   f15({
+//     data: [{ a1: 10, a2: "First!", a3: { num1: [5, 6, 8, 97, 1] }, a4: false }],
+//   })
+// );
+
+// force type
+
+// example 1
+const f14_1 = (args: IF14Args): number =>
+  args.a3.num1.reduce((acc, r) => acc + r, 0);
+// example 2
+const f14_2 = (args: IF14Args) =>
+  args.a3.num1.reduce((acc, r) => acc + r, 0) as number;
+// example 3
+const f14_3 = (args: IF14Args): { a1: number; a2: string } => ({
+  a1: 3,
+  a2: "First!",
+});
+// example 4
+const f14_4 = (args: IF14Args) =>
+  ({ a1: 3, a2: "First!" } as { a1: number; a2: string });
+
+// example 5 สร้าง interface มาบังคับ types
+interface IF14Result {
+  a1: number;
+  a2: string;
+}
+
+const f14_5 = (args: IF14Args): IF14Result => ({ a1: 3, a2: "First!" });
+
+// example 6
+const f14_6 = (args: IF14Args) => ({ a1: 3, a2: "Second!" } as IF14Result);
