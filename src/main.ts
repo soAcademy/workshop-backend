da7234549f4bb3861f5e1bacee664a5756e2870c
Phisit Siprasatthong <phisite@gmail.com>
2023-02-22 15:06:18 +0700

    feat: add partial solution for simpleApi #6_codec (manual input type checking for helloOrder function)

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,8 @@ const isString = (data: any) => typeof data === "string";
 
 const isObject = (data: any) => typeof data === "object";
 
+const isArray = (data: any) => Array.isArray(data);
+
 const app: Application = express();
 
 app.use(express.json());
@@ -126,17 +128,35 @@ app.post("/function/helloMultiply", (req: Request, res: Response) => {
 
 app.post("/function/helloReduce", (req: Request, res: Response) => {
   const body = req?.body;
-  const result = helloReduce({
-    name: body?.name,
-    numbers: body?.numbers,
-  });
-  res.status(200).json(result);
+  if (
+    isString(body?.name) &&
+    isArray(body?.numbers) &&
+    body?.numbers.every(isNumber)
+  ) {
+    const result = helloReduce({
+      name: body?.name,
+      numbers: body?.numbers,
+    });
+    res.status(200).json(result);
+  } else {
+    res.status(500).json({ error: "ERROR: invalid request (manual codec)" });
+  }
 });
 
 app.post("/function/helloOrder", (req: Request, res: Response) => {
   const body = req?.body;
-  const result = helloOrder(body);
-  res.status(200).json(result);
+  if (
+    isString(body?.name) &&
+    isArray(body?.orders) &&
+    body?.orders.every(isObject) &&
+    body?.orders.every((order: any) => isString(order.product)) &&
+    body?.orders.every((order: any) => isNumber(order.price))
+  ) {
+    const result = helloOrder(body);
+    res.status(200).json(result);
+  } else {
+    res.status(500).json({ error: "ERROR: invalid request (manual codec)" });
+  }
 });
 
 app.listen(8000, () => {
