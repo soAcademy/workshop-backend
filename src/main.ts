96048cb85de2e977e401eafc0b0889fffe900f94
BornDhamachart <dhamachart.c@gmail.com>
2023-02-22 10:21:11 +0700

    feat:Submit workshop

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,25 @@
-import express, { Application, Request, Response } from "express";
-import axios from "axios";
+const f1 = (x: number, y: number) => Math.min(x, y);
+const f2 = (x: string, y: string) => x + y;
+const f3 = (x: boolean, y: number) => (x ? y : -y);
+const f4 = (x: Date) => new Date(x).toLocaleDateString();
+const f5 = (x: number[]) => x.reduce((acc, r) => acc + r, 0);
+const f6 = (x: string[]) => x.join("###");
+const f7 = (x: { a1: number; a2: number }) => x.a1 + x.a2;
+const f8 = (x: { a1: number; a2: number[] }) =>
+  x.a1 + x.a2.reduce((acc, r) => acc + r, 0);
+const f9 = (x: { a1: string; a2: number }[]) => x[0].a2 + x[1].a2;
 
-const app: Application = express();
-
-app.use(express.json());
-
-app.get("/get/nested1", (req: Request, res: Response) => {
-  res.status(200).send("Nested1");
-});
-
-app.get("/get/nested2/:id", (req: Request, res: Response) => {
-  const params = req?.params;
-  console.log(params);
-  res.status(200).send(`id:${params?.id}`);
-});
-
-app.get("/get/nested2/:id/:title", (req: Request, res: Response) => {
-  const params = req?.params;
-  console.log(params);
-  res.status(200).send(`id:${params?.id}, title:${params?.title}`);
-});
-
-app.listen(3100, () => {
-  console.log("Server start on port 3100!");
-});
+console.log(f1(1, 5));
+console.log(f2("hello", 3));
+console.log(f3(true, 5));
+console.log(f4(new Date()));
+console.log(f5([1, 2, 3, 4, 5]));
+console.log(f6(["hello", "bin", "world"]));
+console.log(f7({ a1: 10, a2: 20 }));
+console.log(f8({ a1: 10, a2: [20, 30, 40] }));
+console.log(
+  f9([
+    { a1: "bin", a2: 10 },
+    { a1: "world", a2: 20 },
+  ])
+);
---
+++ src/simpleApi/main5.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.get("/get/nested1", (req: Request, res: Response) => {
+  res.status(200).send("Nested1");
+});
+
+app.get("/get/nested2/:id", (req: Request, res: Response) => {
+  const params = req?.params;
+  console.log(params);
+  res.status(200).send(`id:${params?.id}`);
+});
+
+app.get("/get/nested2/:id/:title", (req: Request, res: Response) => {
+  const params = req?.params;
+  console.log(params);
+  res.status(200).send(`id:${params?.id}, title:${params?.title}`);
+});
+
+app.listen(3100, () => {
+  console.log("Server start on port 3100!");
+});
