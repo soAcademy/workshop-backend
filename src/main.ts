6674f786a293ecd41ea96099abdc263b6d18e41d
Phisit Siprasatthong <phisite@gmail.com>
2023-02-22 11:59:42 +0700

    feat: add partial solution for simpleApi #6 (TS helloSum function)

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ const helloAt = (args: { name: string; location: string }) => ({
   createdAt: new Date(),
 });
 
+const helloSum = (args: {
+  name: string;
+  number: { x: number; y: number; z: number };
+}) => ({
+  text: `Hello ${args.name} sum ${
+    args.number.x + args.number.y + args.number.z
+  }`,
+  createdAt: new Date(),
+});
+
 const app: Application = express();
 
 app.use(express.json());
@@ -21,6 +31,15 @@ app.post("/function/add", (req: Request, res: Response) => {
 app.post("/function/helloAt", (req: Request, res: Response) => {
   const body = req?.body;
   const result = helloAt({ name: body?.name, location: body?.location });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloSum", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloSum({
+    name: body?.name,
+    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
+  });
   res.status(200).json(result);
 });
 
