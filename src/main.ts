9029789d8198e2f5e12366daa2ad00bb5c08e2fb
Firstweera <firstweera357@gmail.com>
2023-02-22 10:25:21 +0700

    feat: type

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,146 @@
 import express, { Application, Request, Response } from "express";
 
+// สร้าง function
+
+const add = (x: number, y: number) => x + y;
+
+const helloAt = (args: { name: string; location: string }) => ({
+  text: `Hello ${args.name} at ${args.location}`,
+  createdAt: new Date(),
+});
+
+const helloSum = (args: {
+  name: string;
+  number: { x: number; y: number; z: number };
+}) => ({
+  text: `Hello ${args.name} sum ${
+    args.number.x + args.number.y + args.number.z
+  }`,
+  createdAt: new Date(),
+});
+
+interface IHelloMultiply {
+  name: string;
+  number: { x: number; y: number; z: number };
+}
+
+const helloMultiply = (args: IHelloMultiply) => ({
+  text: `Hello ${args.name} multiply ${
+    args.number.x * args.number.y * args.number.z
+  }`,
+  createdAt: new Date(),
+});
+
+interface IHelloReduce {
+  name: string;
+  numbers: number[];
+}
+
+const helloReduce = (args: IHelloReduce) => {
+  const result = args.numbers.reduce((acc, r) => acc + r, 0);
+  return {
+    text: `hello ${args.name} reduce ${result}`,
+    createdAt: new Date(),
+  };
+};
+
+interface IHelloOrder {
+  name: string;
+  orders: {
+    product: string;
+    price: number;
+  }[];
+}
+
+const helloOrder = (args: IHelloOrder) => {
+  const result = args.orders.reduce((acc, r) => acc + r.price, 0);
+  return {
+    text: `Hello ${args.name} order ${result}`,
+    createdAt: new Date(),
+  };
+};
+
+// Express Server
+
 const app: Application = express();
 
-app.use(express.json()); // ตัวกำหนดให้ express เห็นข้อมูลเป็น json
+app.use(express.json());
 
-app.get("/get/nested1", (req: Request, res: Response) => {
-  res.status(200).send("Nested1");
+app.post("/function/add", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = add(body?.x, body?.y);
+  res.status(200).send(`Result: ${result}`);
 });
 
-app.get("/get/nested2/:id", (req: Request, res: Response) => {
-  const params = req?.params;
-  console.log(params);
-  res.status(200).send(`id: ${params.id}`);
+app.post("/function/helloAt", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloAt({
+    name: body?.name,
+    location: body?.location,
+  });
+  res.status(200).json(result);
 });
 
-app.get("/get/nested3/:id/:title", (req: Request, res: Response) => {
-  const params = req?.params;
-  console.log(params);
-  res.status(200).send(`id: ${params.id}, title: ${params.title}`);
+app.post("/function/helloSum", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloSum({
+    name: body?.name,
+    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloMultiply", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloMultiply({
+    name: body?.name,
+    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloReduce", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloReduce({
+    name: body?.name,
+    numbers: body?.numbers,
+  });
+  res.status(200).json(result);
+});
+
+app.post("/function/helloOrder", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloOrder(body);
+  res.status(200).json(result);
 });
 
 app.listen(3000, () => {
   console.log("Server start on port 3000!");
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+const f1 = (x: number, y: number) => Math.min(x, y);
+const f2 = (x: string, y: string) => x + y;
+const f3 = (x: boolean, y: number) => (x === true ? y : -y);
+const f4 = (x: Date) => new Date(x).toLocaleDateString();
+const f5 = (x: number[]) => x.reduce((acc, r) => acc + r, 0);
+const f6 = (x: string[]) => x.join(" ->  ");
+const f7 = (x: { a1: number; a2: number }) => x.a1 + x.a2;
+const f8 = (x: { a1: number; a2: number[] }) =>
+  x.a1 + x.a2.reduce((acc, r) => acc + r, 0);
+const f9 = (x: { a1: string; a2: number }[]) => x[1].a2 + x[1].a1;
+
+console.log(f1(2, 6));
+console.log(f2("Hello", " World!!"));
+console.log(f3(false, 26));
+console.log(f4(new Date()));
+console.log(f5([1, 2, 3, 4, 5]));
+console.log(f6(["1", "2", "3", "4", "5"]));
+console.log(f7({ a1: 5, a2: 6 }));
+console.log(f8({ a1: 7, a2: [8, 9, 10] }));
+console.log(
+  f9([
+    { a1: "Meow!!", a2: 8 },
+    { a1: "First", a2: 9 },
+  ])
+);
