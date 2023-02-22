f7161362f1a594b51e3137110f465e406759a25a
Panasun Sunanta <panasunsunanta@gmail.com>
2023-02-22 10:08:25 +0700

    feat: practice type script

--- src/main.ts
+++ src/main.ts
@@ -9,34 +9,74 @@ const app: Application = express();
 app.use(express.json());
 
 app.get("/get/nested1", (req: Request, res: Response) => {
-   res.status(200).send("Nested1")
+  res.status(200).send("Nested1");
 });
 
-
 app.get("/get/nested2/:id", (req: Request, res: Response) => {
-  const params = req?.params
-  console.log(params)
-  res.status(200).send(`id: ${params?.id}`)
+  const params = req?.params;
+  console.log(params);
+  res.status(200).send(`id: ${params?.id}`);
 });
 
 app.get("/get/nested3/:id/:title", (req: Request, res: Response) => {
-  const params = req?.params
-  console.log(params)
+  const params = req?.params;
+  console.log(params);
   // res.status(200).send(`id: ${params?.id}, title: ${params?.title}`)
   const obj1 = {
     a1: 1,
     a2: 2,
-  }
+  };
   const obj2 = {
     b1: 11,
     b2: 22,
-  }
-  res.status(200).json([
-    obj1,
-    obj2
-  ]);
+  };
+  console.log(typeof new Date());
+  res.status(200).json([obj1, obj2]);
 });
 
 app.listen(3000, () => {
   console.log("Server start on port 3000!");
 });
+
+// validateType if / else (runtime type checking)
+// (a: number, b: number) (runtime, external world)
+// io-ts
+// [APPPPPP
+//  f1(a1: number, b1: number) -> (string, number)
+//  f2(a2: string, b2: string) -> (number, number)
+//  f3(a3: number, b3: number) -> (number)
+// ] -> ()
+
+// Runtime typechecking
+// try {
+//   runTimeTypeChecking(req.body);
+//   process(req.body)
+// } catch (e) {
+//   send(e)
+// }
+
+const f1 = (x: number, y: number) => Math.min(x, y);
+const f2 = (x: string, y: string) => x + y;
+const f3 = (x: boolean, y: number) => (x ? y : -y);
+const f4 = (x: Date) => new Date(x).toLocaleDateString();
+const f5 = (x: number[]) => x.reduce((acc, r) => acc + r, 0);
+const f6 = (x: string[]) => x.join("###");
+const f7 = (x: { a1: number; a2: number }) => x.a1 + x.a2;
+const f8 = (x: { a1: number; a2: number[] }) =>
+  x.a1 + x.a2.reduce((acc, r) => acc + r, 0);
+const f9 = (x: { a1: string; a2: number }[]) => x[0].a2 + x[1].a2;
+
+console.log(f1(1, 5));
+console.log(f2("hello", "bin"));
+console.log(f3(true, 5));
+console.log(f4(new Date()));
+console.log(f5([1, 2, 3, 4, 5]));
+console.log(f6(["hello", "bin", "world"]));
+console.log(f7({ a1: 10, a2: 20 }));
+console.log(f8({ a1: 10, a2: [20, 30, 40] }));
+console.log(
+  f9([
+    { a1: "bin", a2: 10 },
+    { a1: "world", a2: 20 },
+  ])
+);
--- src/simpleApiAnswer/main6.ts
+++ src/simpleApiAnswer/main6.ts
@@ -1,124 +1,82 @@
 import express, { Application, Request, Response } from "express";
+import axios from "axios";
 
-// สร้าง function
-
-// Hello Add
-const add = (x: number, y: number) => x + y;
-
-// Hello At
-const helloAt = (args: { name: string; location: string }) => ({
-  text: `Hello ${args.name} at ${args.location}`,
-  createdAt: new Date(),
-});
-
-// Hello Sum
-const helloSum = (args: {
-  name: string;
-  number: { x: number; y: number; z: number };
-}) => ({
-  text: `Hello ${args.name} sum ${
-    args.number.x + args.number.y + args.number.z
-  }`,
-  createdAt: new Date(),
-});
-
-// Hello Multiply
-interface IHelloMultiply {
-  name: string;
-  number: { x: number; y: number; z: number };
-}
-
-const helloMultiply = (args: IHelloMultiply) => ({
-  text: `Hello ${args.name} multiply ${
-    args.number.x * args.number.y * args.number.z
-  }`,
-  createdAt: new Date(),
-});
-
-// Hello Reduce
-interface IHelloReduce {
-  name: string;
-  numbers: number[];
-}
-
-const helloReduce = (args: IHelloReduce) => {
-  const result = args.numbers.reduce((acc, r) => acc + r, 0);
-  return {
-    text: `Hello ${args.name} reduce ${result}`,
-    createdAt: new Date(),
-  };
-};
-
-// Hello Order
-interface IHelloOrder {
-  name: string;
-  orders: {
-    product: string,
-    price: number
-  }[];
-}
-
-const helloOrder = (args: IHelloOrder) => {
-  const result = args.orders.reduce((acc, r) => acc + r.price, 0);
-  return {
-    text: `Hello ${args.name} order ${result}`,
-    createdAt: new Date(),
-  };
-};
-
-// Express Server
 const app: Application = express();
 
+// สร้าง Nested Path
+
+// ต้องเพิ่มบรรทัดนี้เพื่อให้ POST method รับข้อมูลแบบ json ได้
 app.use(express.json());
 
-app.post("/function/add", (req: Request, res: Response) => {
-  const body = req?.body;
-  const result = add(body?.x, body?.y);
-  res.status(200).send(`Result: ${result}`);
+app.get("/get/nested1", (req: Request, res: Response) => {
+  res.status(200).send("Nested1");
 });
 
-app.post("/function/helloAt", (req: Request, res: Response) => {
-  const body = req?.body;
-  const result = helloAt({
-    name: body?.name,
-    location: body?.location,
-  });
-  res.status(200).json(result);
+app.get("/get/nested2/:id", (req: Request, res: Response) => {
+  const params = req?.params;
+  console.log(params);
+  res.status(200).send(`id: ${params?.id}`);
 });
 
-app.post("/function/helloSum", (req: Request, res: Response) => {
-  const body = req?.body;
-  const result = helloSum({
-    name: body?.name,
-    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
-  });
-  res.status(200).json(result);
-});
-
-app.post("/function/helloMultiply", (req: Request, res: Response) => {
-  const body = req?.body;
-  const result = helloMultiply({
-    name: body?.name,
-    number: { x: body?.number.x, y: body?.number.y, z: body?.number.z },
-  });
-  res.status(200).json(result);
-});
-
-app.post("/function/helloReduce", (req: Request, res: Response) => {
-  const body = req?.body;
-  const result = helloReduce({
-    name: body?.name,
-    numbers: body?.numbers,
-  });
-  res.status(200).json(result);
-});
-
-app.post("/function/helloOrder", (req: Request, res: Response) => {
-  const body = req?.body;
-  const result = helloOrder(body);
-  res.status(200).json(result);
+app.get("/get/nested3/:id/:title", (req: Request, res: Response) => {
+  const params = req?.params;
+  console.log(params);
+  // res.status(200).send(`id: ${params?.id}, title: ${params?.title}`)
+  const obj1 = {
+    a1: 1,
+    a2: 2,
+  };
+  const obj2 = {
+    b1: 11,
+    b2: 22,
+  };
+  console.log(typeof new Date());
+  res.status(200).json([obj1, obj2]);
 });
 
 app.listen(3000, () => {
   console.log("Server start on port 3000!");
 });
+
+// validateType if / else (runtime type checking)
+// (a: number, b: number) (runtime, external world)
+// io-ts
+// [APPPPPP
+//  f1(a1: number, b1: number) -> (string, number)
+//  f2(a2: string, b2: string) -> (number, number)
+//  f3(a3: number, b3: number) -> (number)
+// ] -> ()
+
+// Runtime typechecking
+// try {
+//   runTimeTypeChecking(req.body);
+//   process(req.body)
+// } catch (e) {
+//   send(e)
+// }
+
+const f1 = (x: number, y: number) => Math.min(x, y);
+const f2 = (x: string, y: string) => x + y;
+const f3 = (x: boolean, y: number) => (x ? y : -y);
+const f4 = (x: Date) => new Date(x).toLocaleDateString();
+const f5 = (x: number[]) => x.reduce((acc, r) => acc + r, 0);
+const f6 = (x: string[]) => x.join("###");
+const f7 = (x: { a1: number; a2: number }) => x.a1 + x.a2;
+const f8 = (x: { a1: number; a2: number[] }) =>
+  x.a1 + x.a2.reduce((acc, r) => acc + r, 0);
+const f9 = (x: { a1: string; a2: number }[]) => x[0].a2 + x[1].a2;
+
+console.log(f1(1, 5));
+console.log(f2("hello", "bin"));
+console.log(f3(true, 5));
+console.log(f4(new Date()));
+console.log(f5([1, 2, 3, 4, 5]));
+console.log(f6(["hello", "bin", "world"]));
+console.log(f7({ a1: 10, a2: 20 }));
+console.log(f8({ a1: 10, a2: [20, 30, 40] }));
+console.log(
+  f9([
+    { a1: "bin", a2: 10 },
+    { a1: "world", a2: 20 },
+  ])
+);
