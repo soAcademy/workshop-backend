30ded355ae0910d61ddb25f18c5cb95fcbdd36f4
Phisit Siprasatthong <phisite@gmail.com>
2023-02-22 11:42:36 +0700

    feat: add partial solution for simpleApi #6 (TS helloAt function)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import express, { Application, Request, Response } from "express";
 
 const add = (x: number, y: number) => x + y;
 
+const helloAt = (args: { name: string; location: string }) => ({
+  text: `Hello ${args.name} at ${args.location}`,
+  createdAt: new Date(),
+});
+
 const app: Application = express();
 
 app.use(express.json());
@@ -11,6 +16,12 @@ app.post("/function/add", (req: Request, res: Response) => {
   const body = req?.body;
   const result = add(body?.x, body?.y);
   res.status(200).send(`Result: ${result}`);
+});
+
+app.post("/function/helloAt", (req: Request, res: Response) => {
+  const body = req?.body;
+  const result = helloAt({ name: body?.name, location: body?.location });
+  res.status(200).json(result);
 });
 
 app.listen(8000, () => {
