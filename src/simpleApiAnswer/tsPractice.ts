62433aa2455912d6980980e32c8ab08b24b66e71
korana <korayut@gmail.com>
2023-02-22 11:02:31 +0700

    feat: add interface for ts

--- src/simpleApiAnswer/tsPractice.ts
+++ src/simpleApiAnswer/tsPractice.ts
@@ -32,6 +32,13 @@ console.log(
   ])
 );
 
+interface IF10Args {
+  a1: string;
+  a2: number;
+}
+
+const f10 = (x: IF10Args) => x[0].a2 + x[1].a2
+
 // validateType if / else (runtime type checking)
 // (a: number, b: number) (runtime, external world)
 // io-ts
@@ -49,72 +56,3 @@ console.log(
 //   send(e)
 // }
 
-interface IF10Args {
-  a1: string;
-  a2: number;
-}
-
-const f10 = (x: IF10Args[]) => x[0].a2 + x[1].a2;
-
-interface IF11Args {
-  a1: number | string;
-  a2: number | string;
-}
-
-const f11 = (x: IF11Args) => `${x.a1} ${x.a2}`;
-
-interface IF12Args {
-  num1: number[];
-}
-
-const f12 = (args: IF12Args) => args.num1.reduce((acc, r) => acc + r, 0);
-
-interface IF13Args {
-  a1: number;
-  a2: string;
-  a3: IF12Args;
-}
-
-const f13 = (args: IF13Args) => args.a3.num1.reduce((acc, r) => acc + r, 0);
-console.log(f13({ a1: 10, a2: "Kor", a3: { num1: [1, 2, 3] } }));
-
-interface IF14Args extends IF13Args {
-  a4: boolean;
-}
-
-const f14 = (args: IF14Args) => args.a3.num1.reduce((acc, r) => acc + r, 0);
-console.log(
-  f14({
-    a1: 10,
-    a2: "Kor",
-    a3: { num1: [1, 2, 3] },
-    a4: true,
-  })
-);
-
-interface IF15Args {
-  data: IF14Args[]
-}
-
-interface IF14Result {
-  a1: number;
-  a2: string;
-}
-
-const f14_1 = (args: IF14Args): number =>
-  args.a3.num1.reduce((acc, r) => acc + r, 0);
-const f14_2 = (args: IF14Args) =>
-  args.a3.num1.reduce((acc, r) => acc + r, 0) as number;
-const f14_3 = (args: IF14Args): { a1: number; a2: string } => ({
-  a1: 3,
-  a2: "Bin",
-});
-const f14_4 = (args: IF14Args) =>
-  ({ a1: 3, a2: "Bin" } as { a1: number; a2: string });
-
-const f14_5 = (args: IF14Args): IF14Result =>
-({ a1: 3, a2: "Bin" });
-
-
-const f14_6 = (args: IF14Args) =>
-({ a1: 3, a2: "Bin" }) as IF14Result;
