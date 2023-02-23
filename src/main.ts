1a9553b171910d4f4530f9635d52632237f3cf9f
jaiieth <midzty.rb128@gmail.com>
2023-02-23 11:04:32 +0700

    fix: use .map

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,14 @@ import { AppRoutes } from "./routes";
 
 const app: Application = express();
 
-app.use(express.json())
+app.use(express.json());
 
-AppRoutes.forEach((route) => {
+AppRoutes.map((route) => {
   app[route.method as keyof Application](
     route.path,
-    (request: express.Request, response: express.Response) => {
-      route.action(request, response)
-    }
-  )
-})
+    (req: Request, res: Response) => route.action(req, res)
+  );
+});
 
 app.listen(5555, () => {
   console.log("Server started on port 5555");
