f7fcf096837a55e1155c39f938d85c5d377fb7a6
jaiieth <midzty.rb128@gmail.com>
2023-03-05 16:01:22 +0700

    comment some api

--- main.ts
+++ main.ts
@@ -82,12 +82,12 @@ TodolistRoutes.map((route) => {
   );
 });
 
-FoodOrderingRoutes.map((route) => {
-  app[route.method as keyof Application](
-    route.path,
-    (req: Request, res: Response) => route.action(req, res)
-  );
-});
+// FoodOrderingRoutes.map((route) => {
+//   app[route.method as keyof Application](
+//     route.path,
+//     (req: Request, res: Response) => route.action(req, res)
+//   );
+// });
 
 app.listen(5555, () => {
   console.log("Server started on port 5555");
