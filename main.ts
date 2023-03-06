6ff5b31bff2f88b3ba2d19045a531b7d739bd656
jaiieth <midzty.rb128@gmail.com>
2023-03-06 19:38:53 +0700

    fix:cors

--- main.ts
+++ main.ts
@@ -25,10 +25,14 @@ const upload = multer({ storage });
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000, https://sprinttech-earth-kitchen.web.app/"
-  );
+  let allowedOrigins = [
+    "http://localhost:3000",
+    "https://sprinttech-earth-kitchen.web.app/",
+  ];
+  let origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
