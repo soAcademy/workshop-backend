23bc7d98e422c58412186ec05d6c91896af93a0b
Phisit Siprasatthong <phisite@gmail.com>
2023-03-13 10:58:38 +0700

    feat: add tests for getCategories, getQuizzes functions

--- src/binQuiz/binQuiz.spec.ts
+++ src/binQuiz/binQuiz.spec.ts
@@ -1,12 +1,15 @@
 import {
   createCategory,
   createQuiz,
+  getCategories,
+  getQuizzes,
   getQuizzesByCategory,
   sumNumbers,
 } from "./binQuiz.resolvers";
 
 describe("Trivia", () => {
   let category1Id: number;
+
   test("should do a summation of two numbers correctly", () => {
     const result1 = sumNumbers({ number1: 10, number2: 20 });
     const result2 = sumNumbers({ number1: 10, number2: 30 });
@@ -22,6 +25,12 @@ describe("Trivia", () => {
     const result1 = await createCategory({ name: category1 });
     category1Id = result1.id;
     expect(result1.name === category1).toBe(true);
+  });
+
+  it("should get all categories", async () => {
+    const result = await getCategories();
+    console.log("getCategories", result);
+    expect(result.length > 0).toBe(true);
   });
 
   test("should create a new quiz", async () => {
@@ -46,6 +55,12 @@ describe("Trivia", () => {
     const result = await createQuiz(data);
     console.log("createQuiz", result);
     expect(result.questionText === data.questionText).toBe(true);
+  });
+
+  it("should get all quizzes", async () => {
+    const result = await getQuizzes();
+    console.log("getQuizzes", result);
+    expect(result.length > 0).toBe(true);
   });
 
   it("should get all quizzes for a given category", async () => {
