4efe105854e84b2ba8f7b32f420db531f62b0d28
Phisit Siprasatthong <phisite@gmail.com>
2023-03-13 11:46:16 +0700

    feat: add test for createTasks function

--- src/todoList/todoList.spec.ts
+++ src/todoList/todoList.spec.ts
@@ -1,9 +1,24 @@
-import { createTask, getTasks } from "./todoList.resolvers";
+import { createTask, createTasks, getTasks } from "./todoList.resolvers";
 
 describe("Todo list", () => {
   it("should create a new task", async () => {
     const result = await createTask({ task: "test", bgColor: "slate" });
     expect(result.task === "test").toBe(true);
+  });
+
+  it("should create new tasks", async () => {
+    const data = [
+      {
+        task: "good morning",
+        bgColor: "red",
+      },
+      {
+        task: "good afternoon",
+        bgColor: "yellow",
+      },
+    ];
+    const result = await createTasks(data);
+    expect(result.count === data.length).toBe(true);
   });
 
   it("should get all pending and done tasks", async () => {
