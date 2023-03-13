91480f90efc2127de93543337555b8f80013e73a
jaiieth <midzty.rb128@gmail.com>
2023-03-13 15:33:56 +0700

    feat:add handler, routes

--- src/TwitterAPI/handler.ts
+++ src/TwitterAPI/handler.ts
@@ -1,6 +1,32 @@
 import { Request, Response } from "express";
-import { createUserCodec, followCodec, getUserProfileCodec, unFollowCodec } from "./interface";
-import { createUser, follow, getUserProfile, unfollow } from "./resolver";
+import {
+  createTweetCodec,
+  createUserCodec,
+  followCodec,
+  getTweetsByHashtagsCodec,
+  getTweetsByUserCodec,
+  getUserProfileCodec,
+  likeTweetCodec,
+  replyTweetCodec,
+  sendDirectMessageCodec,
+  unFollowCodec,
+  updateUserProfilePictureCodec,
+} from "./interface";
+import {
+  createTweet,
+  createUser,
+  follow,
+  getHashtags,
+  getTweetsByHashtags,
+  getTweetsByUser,
+  getUserProfile,
+  likeTweet,
+  replyTweet,
+  sendDirectMessage,
+  unfollow,
+  unlikeTweet,
+  updateUserProfilePicture,
+} from "./resolver";
 
 export const createUserHandler = (req: Request, res: Response) => {
   try {
@@ -47,7 +73,7 @@ export const followHandler = (req: Request, res: Response) => {
   }
 };
 
-export const unFollowingHandler = (req: Request, res: Response) => {
+export const unFollowHandler = (req: Request, res: Response) => {
   try {
     const body = req?.body;
     if (unFollowCodec.decode(body)._tag === "Right") {
@@ -62,3 +88,135 @@ export const unFollowingHandler = (req: Request, res: Response) => {
   }
 };
 
+export const createTweetHandler = (req: Request, res: Response) => {
+  try {
+    const body = req?.body;
+    if (createTweetCodec.decode(body)._tag === "Right") {
+      return createTweet(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getHashtagsHandler = (req: Request, res: Response) => {
+  try {
+    return getHashtags()
+      .then((response) => res.status(200).json(response))
+      .catch((err) => res.status(500).send(err));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getTweetsByHashtagsHandler = (req: Request, res: Response) => {
+  try {
+    const body = req?.body;
+    if (getTweetsByHashtagsCodec.decode(body)._tag === "Right") {
+      return getTweetsByHashtags(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getTweetsByUserHandler = (req: Request, res: Response) => {
+  try {
+    const body = req?.body;
+    if (getTweetsByUserCodec.decode(body)._tag === "Right") {
+      return getTweetsByUser(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const replyTweetHandler = (req: Request, res: Response) => {
+  try {
+    const body = req?.body;
+    if (replyTweetCodec.decode(body)._tag === "Right") {
+      return replyTweet(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const likeTweetHandler = (req: Request, res: Response) => {
+  try {
+    const body = req?.body;
+    if (likeTweetCodec.decode(body)._tag === "Right") {
+      return likeTweet(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const unlikeTweetHandler = (req: Request, res: Response) => {
+  try {
+    const body = req?.body;
+    if (likeTweetCodec.decode(body)._tag === "Right") {
+      return unlikeTweet(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const updateUserProfilePictureHandler = (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const body = req?.body;
+    if (updateUserProfilePictureCodec.decode(body)._tag === "Right") {
+      return updateUserProfilePicture(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const sendDirectMessageHandler = (req: Request, res: Response) => {
+  try {
+    const body = req?.body;
+    if (sendDirectMessageCodec.decode(body)._tag === "Right") {
+      return sendDirectMessage(body)
+        .then((response) => res.status(200).json(response))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      res.status(500).send("Failed To Validate Codec");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- src/TwitterAPI/routes.ts
+++ src/TwitterAPI/routes.ts
@@ -1,4 +1,18 @@
-import { createUserHandler, followHandler, getUserProfileHandler, unFollowingHandler } from "./handler";
+import {
+  createTweetHandler,
+  createUserHandler,
+  followHandler,
+  getHashtagsHandler,
+  getTweetsByHashtagsHandler,
+  getTweetsByUserHandler,
+  getUserProfileHandler,
+  likeTweetHandler,
+  replyTweetHandler,
+  sendDirectMessageHandler,
+  unFollowHandler,
+  unlikeTweetHandler,
+  updateUserProfilePictureHandler,
+} from "./handler";
 
 export const TwitterRoutes = [
   {
@@ -19,6 +33,51 @@ export const TwitterRoutes = [
   {
     method: "post",
     path: "/twitter/unfollow",
-    action: unFollowingHandler,
+    action: unFollowHandler,
   },
-]
+  {
+    method: "post",
+    path: "/twitter/createTweet",
+    action: createTweetHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/getHashtags",
+    action: getHashtagsHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/getTweetsByHashtags",
+    action: getTweetsByHashtagsHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/getTweetsByUser",
+    action: getTweetsByUserHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/replyTweet",
+    action: replyTweetHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/likeTweet",
+    action: likeTweetHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/unlikeTweet",
+    action: unlikeTweetHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/updateUserProfilePicture",
+    action: updateUserProfilePictureHandler,
+  },
+  {
+    method: "post",
+    path: "/twitter/sendDirectMessage",
+    action: sendDirectMessageHandler,
+  },
+];
