9504e4df37f71008d0b824a999d365aedd6ea1f4
Phisit Siprasatthong <phisite@gmail.com>
2023-03-13 11:55:28 +0700

    feat: add tests for updateTaskStatus function

--- src/todoList/todoList.spec.ts
+++ src/todoList/todoList.spec.ts
@@ -1,8 +1,16 @@
-import { createTask, createTasks, getTasks } from "./todoList.resolvers";
+import {
+  createTask,
+  createTasks,
+  getTasks,
+  updateTaskStatus,
+} from "./todoList.resolvers";
 
 describe("Todo list", () => {
+  let taskId: number;
+
   it("should create a new task", async () => {
     const result = await createTask({ task: "test", bgColor: "slate" });
+    taskId = result.id;
     expect(result.task === "test").toBe(true);
   });
 
@@ -25,5 +33,21 @@ describe("Todo list", () => {
     const result = await getTasks();
     console.log("getTasks", result);
     expect(result.length > 0).toBe(true);
+  });
+
+  it("should update a specific task status to DONE", async () => {
+    const result = await updateTaskStatus({
+      id: taskId,
+      status: "DONE",
+    });
+    expect(result.status === "DONE").toBe(true);
+  });
+
+  it("should update a specific task status to DELETED", async () => {
+    const result = await updateTaskStatus({
+      id: taskId,
+      status: "DELETED",
+    });
+    expect(result.status === "DELETED").toBe(true);
   });
 });
